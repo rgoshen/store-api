dfd62687da6ce345a2e03e690b2f4464d30c66e8
rgoshen <rick.goshen@gmail.com>
2021-11-04 17:39:24 -0700

    added query route and filters for getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,38 @@
+const { query } = require('express');
+const Product = require('../models/product');
+
+// test route
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'testing products route' });
+  const products = await Product.find({}).sort('name -price');
+  res.status(200).json({ products, nbHits: products.length });
 };
+
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const products = await Product.find({});
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+const searchForProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  // query string parameter filters
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
   getAllProductsStatic,
   getAllProducts,
+  searchForProducts,
 };
--- routes/products.js
+++ routes/products.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   getAllProducts,
   getAllProductsStatic,
+  searchForProducts,
 } = require('../controllers/products');
 
 router.route('/').get(getAllProducts);
+router.route('/query').get(searchForProducts);
 router.route('/static').get(getAllProductsStatic);
 
 module.exports = router;
