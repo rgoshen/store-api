565f9164eecda7bfcfcbd166da40ff374b494197
rgoshen <rick.goshen@gmail.com>
2021-11-07 15:52:03 -0700

    added sort functionality in searchForProducts

--- controllers/products.js
+++ controllers/products.js
@@ -13,7 +13,7 @@ const getAllProducts = async (req, res) => {
 };
 
 const searchForProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   // query string parameter filters
@@ -27,7 +27,18 @@ const searchForProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
